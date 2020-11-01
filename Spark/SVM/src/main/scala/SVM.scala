215b2d8f6fca8e9e21781deee369636a4c2508bf
Coursal <coursal@espiv.net>
2020-11-02 00:32:42 +0200

    turned tabs to spaces

--- Spark/SVM/src/main/scala/SVM.scala
+++ Spark/SVM/src/main/scala/SVM.scala
@@ -21,77 +21,77 @@ spark-submit --master local ./target/scala-2.12/svm_2.12-0.1.jar
 
 object SVM
 {
-	def main(args: Array[String]): Unit = 
-	{
-		// create a scala spark context for rdd management and a spark session for dataframe management
-		val conf = new SparkConf().setAppName("Support Vector Machines")
-		val sc = new SparkContext(conf)
-		val spark = SparkSession.builder.appName("Support Vector Machines").master("local").getOrCreate()
+    def main(args: Array[String]): Unit = 
+    {
+        // create a scala spark context for rdd management and a spark session for dataframe management
+        val conf = new SparkConf().setAppName("Support Vector Machines")
+        val sc = new SparkContext(conf)
+        val spark = SparkSession.builder.appName("Support Vector Machines").master("local").getOrCreate()
 
         val start_time = System.nanoTime()
 
-		// read the .csv file with the training data 
-		val input = sc.textFile("hdfs://localhost:9000/user/crsl/spark_input_1/tweets.csv")
-						.map(line => line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"))		// split each line to columns..
-						.map(column => 
-									{	// map the cleaned up tweet text as key and sentiment as value
-										(
-				                            column(1).toDouble // set the sentiment of the tweet as key
-				                            ,
-				                            column(3) // set the cleaned up tweet text as value, by cleaning up the text from...
-				                                .replaceAll("(?i)(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})", "")
-				                                .replaceAll("(#|@|&).*?\\w+", "")   // mentions, hashtags, special characters...
-				                                .replaceAll("\\d+", "")             // numbers...
-				                                .replaceAll("[^a-zA-Z ]", " ")      // punctuation...
-				                                .toLowerCase()                      // turn every character left to lowercase...
-				                                .trim()                             // trim the spaces before & after the whole string...
-				                                .replaceAll("\\s+", " ")            // and get rid of double spaces
-										)
-									}
-							)
+        // read the .csv file with the training data 
+        val input = sc.textFile("hdfs://localhost:9000/user/crsl/spark_input_1/tweets.csv")
+                        .map(line => line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"))     // split each line to columns..
+                        .map(column => 
+                                    {   // map the cleaned up tweet text as key and sentiment as value
+                                        (
+                                            column(1).toDouble // set the sentiment of the tweet as key
+                                            ,
+                                            column(3) // set the cleaned up tweet text as value, by cleaning up the text from...
+                                                .replaceAll("(?i)(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})", "")
+                                                .replaceAll("(#|@|&).*?\\w+", "")   // mentions, hashtags, special characters...
+                                                .replaceAll("\\d+", "")             // numbers...
+                                                .replaceAll("[^a-zA-Z ]", " ")      // punctuation...
+                                                .toLowerCase()                      // turn every character left to lowercase...
+                                                .trim()                             // trim the spaces before & after the whole string...
+                                                .replaceAll("\\s+", " ")            // and get rid of double spaces
+                                        )
+                                    }
+                            )
 
 
-		// convert the RDD type sets of training data to Dataframes with named columns in order to apply the TFIDF measure on them
-		import spark.implicits._
-		val input_dataframe = input.toDF("label", "tweet")
+        // convert the RDD type sets of training data to Dataframes with named columns in order to apply the TFIDF measure on them
+        import spark.implicits._
+        val input_dataframe = input.toDF("label", "tweet")
 
 
-		// apply TFIDF to the text of training data to have the proper form of (double, Vectors(double[])) used at the Naive Bayes classifier 
-		val tokenizer = new Tokenizer().setInputCol("tweet").setOutputCol("words")		// split the text of each tweet to tokens
-    	val words_data = tokenizer.transform(input_dataframe)
+        // apply TFIDF to the text of training data to have the proper form of (double, Vectors(double[])) used at the Naive Bayes classifier 
+        val tokenizer = new Tokenizer().setInputCol("tweet").setOutputCol("words")      // split the text of each tweet to tokens
+        val words_data = tokenizer.transform(input_dataframe)
 
-    	val input_hashingTF = new HashingTF().setInputCol("words")setOutputCol("rawFeatures")	// calculate TF
-      	val input_featurized_data = input_hashingTF.transform(words_data)
+        val input_hashingTF = new HashingTF().setInputCol("words")setOutputCol("rawFeatures")   // calculate TF
+        val input_featurized_data = input_hashingTF.transform(words_data)
 
-	    val input_idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")			// calculate IDF
-	    val input_idf_model = input_idf.fit(input_featurized_data)
+        val input_idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")           // calculate IDF
+        val input_idf_model = input_idf.fit(input_featurized_data)
 
-	    val input_rescaled_data = input_idf_model.transform(input_featurized_data)				// calculate TFIDF
-	    //input_rescaled_data.select("label", "features").show()
+        val input_rescaled_data = input_idf_model.transform(input_featurized_data)              // calculate TFIDF
+        //input_rescaled_data.select("label", "features").show()
 
-	 	val Array(training_data, test_data) = input_rescaled_data.randomSplit(Array(0.8, 0.2), seed = 1234L)
+        val Array(training_data, test_data) = input_rescaled_data.randomSplit(Array(0.8, 0.2), seed = 1234L)
 
-		// create the SVM model, train it with the train data and classify/predict the test data 
-	 	val lsvc = new LinearSVC().setMaxIter(10).setRegParam(0.1)
-	    val lsvc_model = lsvc.fit(training_data)
+        // create the SVM model, train it with the train data and classify/predict the test data 
+        val lsvc = new LinearSVC().setMaxIter(10).setRegParam(0.1)
+        val lsvc_model = lsvc.fit(training_data)
 
-	    val predictions = lsvc_model.transform(test_data)
-	    //predictions.show()	// select example rows of predictions to display
-	    val end_time = System.nanoTime()
+        val predictions = lsvc_model.transform(test_data)
+        //predictions.show()    // select example rows of predictions to display
+        val end_time = System.nanoTime()
 
-	    // select each (prediction, true label) set and compute the test error, convert them to RDD, and use the MulticlassMetrics class
-	    // to output the confussion matrix and some metrics
-	    val prediction_and_labels = predictions.select("prediction", "label").rdd.map(r => (r.getDouble(0), r.getDouble(1)))
+        // select each (prediction, true label) set and compute the test error, convert them to RDD, and use the MulticlassMetrics class
+        // to output the confussion matrix and some metrics
+        val prediction_and_labels = predictions.select("prediction", "label").rdd.map(r => (r.getDouble(0), r.getDouble(1)))
 
-    	val metrics = new MulticlassMetrics(prediction_and_labels)
-    	println(metrics.confusionMatrix)
-    	println("ACCURACY: " + metrics.accuracy)
-    	println("F1 SCORE: " + metrics.weightedFMeasure)
-    	println("PRECISION: " + metrics.weightedPrecision)
-    	println("SENSITIVITY: " + metrics.weightedTruePositiveRate)
-    	println("EXECUTION DURATION: " + (end_time - start_time) / 1000000000F + " seconds");
+        val metrics = new MulticlassMetrics(prediction_and_labels)
+        println(metrics.confusionMatrix)
+        println("ACCURACY: " + metrics.accuracy)
+        println("F1 SCORE: " + metrics.weightedFMeasure)
+        println("PRECISION: " + metrics.weightedPrecision)
+        println("SENSITIVITY: " + metrics.weightedTruePositiveRate)
+        println("EXECUTION DURATION: " + (end_time - start_time) / 1000000000F + " seconds");
 
-	    spark.stop()
-		sc.stop()	
-	}
+        spark.stop()
+        sc.stop()   
+    }
 }
